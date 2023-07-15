c84b28bb3e510847aaeb65ea3683c0f302c2f169
Vukantisrinija <139311200+Vukantisrinija@users.noreply.github.com>
2023-07-15 09:54:38 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -33,7 +33,20 @@ $(document).ready(function(){
         $('.menu-btn i').toggleClass("active");
     });
 
-   
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["FRESHER"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Student"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
 
     // owl carousel script
     $('.carousel').owlCarousel({
